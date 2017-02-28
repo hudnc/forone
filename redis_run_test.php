c55f7340ec3988c51075c1a533dc6d2094f96b3c
Cheng <hudnc@qq.com>
2017-02-28 16:31:52 +0800

    测试Redis是否运行在服务器

---
+++ redis_run_test.php
@@ -0,0 +1,13 @@
+<?php
+	//判断Redis 是否在服务器运行
+	function RedisTest(){
+		$token = md5(microtime());
+		$redis = new Redis();
+		$redis->set('test',$token);
+		if($token===$redis->get('test')){
+			echo 'Redis running...'
+		}else{
+			echo 'Redis error...';
+		}
+	}
+?>
